fe73e6dca88b58d8c08f29ef49892a68064f31d5
Christopher Williams <cw@chriswillia.ms>
2019-09-06 10:45:55 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className='App'>
+      {/* content goes here */}
+      <p>Hello, world!</p>
+    </main>
+  );
+}
+
+export default App;
