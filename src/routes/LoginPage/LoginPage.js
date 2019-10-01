a4c1f0a6883cc1ce909cff2832d9fe9c57481567
Christopher Williams <cw@chriswillia.ms>
2019-10-01 18:20:19 -0500

    Overhauls RegisterForm logic, adds blurb about demo account to LoginPage

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -19,6 +19,8 @@ class LoginPage extends React.Component {
                 <h2>Sign in to MMS Photo Board</h2>
                 <LoginForm redirectOnLoginSuccess={this.redirectOnLoginSuccess} />
                 <p>Don't have an account? <Link to='/SignUp'>Create one!</Link></p>
+                <p className='demo_explainer'>Demo account:<br />user: demo<br />password: Demo!23</p>
+                <p className='demo_explainer'>Note that some key functionality of this app is found in SMS / MMS interaction that isn't available with demo credentials. For the full impact of this app, consider registering an account. It's quick and easy!</p>
             </section>
         );
     }
--- src/routes/RegisterPage/RegisterPage.js
+++ src/routes/RegisterPage/RegisterPage.js
@@ -14,10 +14,8 @@ class RegisterPage extends React.Component {
         return (
             <section className='RegisterPage'>
                 <h2>Sign up for MMS Photo Board</h2>
-                <p className='RegisterPage__explainer'>
-                    <em>
-                    MMS Photo Board will not abuse your phone. You will receive a text message to verify your phone number and a reply to messages you send. That's it. Phone numbers are stored securely like a password so they are not readable.
-                    </em>
+                <p>
+                    MMS Photo Board will not abuse your phone. You will receive a text message to verify your phone number and a reply to messages you send. That's it.
                 </p>
                 <RegisterForm redirectOnSuccess={this.redirectOnUserCreationSuccess} />
                 <p>Already have an account? <Link to='/Login'>Login</Link></p>
